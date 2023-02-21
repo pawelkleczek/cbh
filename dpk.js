2cd21d8372f62ba101455cdd2fc8d0285287d0ec
Pawel Kleczek <pakleczek@pm.me>
2023-02-21 19:19:01 +0100

    remove let and instantiate candidate during first assignment

--- dpk.js
+++ dpk.js
@@ -3,25 +3,19 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
   if (!event) {
     return TRIVIAL_PARTITION_KEY;
   }
 
-  if (event.partitionKey) {
-    candidate = event.partitionKey;
-  } else {
-    const data = JSON.stringify(event);
-    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-  }
+  const candidate = event.partitionKey || crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
 
   if (typeof candidate !== "string") {
-    candidate = JSON.stringify(candidate);
+    return JSON.stringify(candidate);
   }
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    return crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
 
   return candidate;
