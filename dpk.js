322a7236f20fc5e5a19eb015efa3e093507cd904
Pawel Kleczek <pakleczek@pm.me>
2023-02-21 19:19:01 +0100

    create util hash, create consts for algorith, move consts out of the function

--- dpk.js
+++ dpk.js
@@ -1,22 +1,28 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
+const ENCODING = 'hex'
+const ALGORITHM = 'sha3-512'
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
 
+exports.deterministicPartitionKey = (event) => {
   if (!event) {
     return TRIVIAL_PARTITION_KEY;
   }
 
-  const candidate = event.partitionKey || crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
+  const candidate = event.partitionKey || hash(JSON.stringify(event));
 
   if (typeof candidate !== "string") {
     return JSON.stringify(candidate);
   }
 
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    return crypto.createHash("sha3-512").update(candidate).digest("hex");
+    return hash(candidate);
   }
 
   return candidate;
 };
+
+function hash(fragment) {
+  return crypto.createHash(ALGORITHM).update(fragment).digest(ENCODING)
+}
